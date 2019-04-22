d6c3a5cd94fabb7b24e1c98de16497bd48027ae7
徐亚军 <jeco_xu@foxmail.com>
2019-04-22 17:27:25 +0800

    SRXXXXfixbug 修复

--- dao/src/main/java/com/itheima/dao/ItemsDao.java
+++ dao/src/main/java/com/itheima/dao/ItemsDao.java
@@ -2,10 +2,13 @@ package com.itheima.dao;
 
 import com.itheima.domain.Items;
 
+import java.beans.Introspector;
+
 /*
  * @Author:  Yajun_Xu
  * @Create: 2018/11/27 13:00
  **/
 public interface ItemsDao {
     Items findItmesById(Integer i);
+    Items findOne(Introspector introspector);
 }
