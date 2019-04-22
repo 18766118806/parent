c4277ab6c89e7d68a0b0052e1d8a6d697d6271bc
徐亚军 <jeco_xu@foxmail.com>
2019-04-22 19:44:35 +0800

    SRXXX--

---
+++ dao/src/main/java/com/itheima/common/MyCommom.java
@@ -0,0 +1,11 @@
+package com.itheima.common;
+
+/*
+ * @Author:  Yajun_Xu
+ * @Create: 2019-04-22 19:27
+ **/
+public class MyCommom {
+    public static void main(String[] args) {
+        System.out.println ("HelloGit");
+    }
+}
